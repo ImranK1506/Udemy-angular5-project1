f1421297449517ce00281c9ebd83f02eae027107
Imran Khan <imran.khan@kpn.com>
2018-04-25 21:12:02 +0200

    added recipe details with an array of Ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,33 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe-list/recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('Recipe One', 'Random description', 'https://loremflickr.com/320/240'),
-        new Recipe('Recipe Two', 'Random description', 'https://loremflickr.com/320/240')
+        new Recipe
+        (
+            'Kitten Cafe Recipe',
+            'Amazing Recipe',
+            'https://loremflickr.com/320/240',
+            [
+                new Ingredient('Sugar', 1),
+                new Ingredient('Spice', 1),
+                new Ingredient('Something Nice', 1)
+            ]
+        ),
+        new Recipe
+        (
+            'Kitten Cafe Recipe Two',
+            'Amazing Recipe 2',
+            'https://loremflickr.com/320/240',
+            [
+                new Ingredient('Sugar', 1),
+                new Ingredient('Spice', 1),
+                new Ingredient('Something Fluffy', 1)
+            ]
+        )
     ];
 
     getRecipes() {
